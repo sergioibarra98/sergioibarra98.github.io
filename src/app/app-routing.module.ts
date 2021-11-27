36c0905a5b6ae613db3218e0c8cb4a2c4d3db2dc
Sergio <sergio.ibarra2398@gmail.com>
2021-11-27 18:12:29 +0100

    Configured router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component'
+import { Error404Component } from './components/error404/error404.component'
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: '**', component: Error404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
