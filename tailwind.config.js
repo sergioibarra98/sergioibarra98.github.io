edf1e6bce9321bd45d911089a3392165a1e65a68
Sergio <sergio.ibarra2398@gmail.com>
2022-01-25 17:01:06 +0100

    Migrated to Tailwind@3.0.15

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,13 @@
 module.exports = {
     prefix: '',
-    purge: ["./src/**/*.{html,ts}"],
-    darkMode: 'class', // or 'media' or 'class'
+    content: [
+      './src/**/*.{html,ts}'
+    ],
     theme: {
       extend: {},
       fontFamily: {
         title: ['Oswald']
       }
-    },
-    variants: {
-      width: ["responsive", "hover", "focus"],
-      extend: {},
     },
     plugins: [require('@tailwindcss/forms'),require('@tailwindcss/typography')],
 };
