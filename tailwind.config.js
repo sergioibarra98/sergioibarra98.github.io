b20ee74200e2d9bc8821e119e2f049877414ce6a
Sergio <sergio.ibarra2398@gmail.com>
2022-01-25 15:45:28 +0100

    Added programs

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,12 @@ module.exports = {
     darkMode: 'class', // or 'media' or 'class'
     theme: {
       extend: {},
+      fontFamily: {
+        title: ['Oswald']
+      }
     },
     variants: {
+      width: ["responsive", "hover", "focus"],
       extend: {},
     },
     plugins: [require('@tailwindcss/forms'),require('@tailwindcss/typography')],
