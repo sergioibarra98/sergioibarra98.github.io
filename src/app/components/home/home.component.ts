8874f14e968eceb6b0e6b19d6408ab15b98159e8
Sergio <sergio.ibarra2398@gmail.com>
2022-01-26 17:46:24 +0100

    Added Tooltip & Blur in the application

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import tippy, { animateFill } from 'tippy.js';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,10 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    tippy('[data-tippy-content]', {
+      placement: "bottom",
+      arrow: false,
+    });
   }
 
 }
