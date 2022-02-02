ad05ee826913fcff7b0c1147c2616d0cef8103e5
sergioibarra98 <sergioibarra98>
2022-02-02 19:57:33 +0100

    Added application component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { Error404Component } from './components/error404/error404.component';
+import { ApplicationComponent } from './src/app/components/subcomponents/application/application.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    Error404Component
+    Error404Component,
+    ApplicationComponent
   ],
   imports: [
     BrowserModule,
